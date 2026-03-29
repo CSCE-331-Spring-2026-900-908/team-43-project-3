e17def6d05d618220c3beee61917b5eee4e1d8e6
ColinXu123 <colinxu2006@gmail.com>
2026-03-29 18:46:38 -0500

    add functionality for cashier pos

--- client/src/pages/CashierPOS.jsx
+++ client/src/pages/CashierPOS.jsx
@@ -1,3 +1,17 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../api";
+
+export default function CashierPOS() {
+  const navigate = useNavigate();
+  const [menu, setMenu] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [activeCat, setActiveCat] = useState("All");
+  const [order, setOrder] = useState([]);
+  const [modifiers, setModifiers] = useState([]);
+  const [customizing, setCustomizing] = useState(null);
+  const [choices, setChoices] = useState({});
+  const [qty, setQty] = useState(1);
+  const [search, setSearch] = useState("");
+
+}
